c14025e774b0a7e20be4a416260d1ec0560504db
Kelsey Fecho <kfecho30@gmail.com>
2018-07-25 13:56:20 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
+  <ClickityClick />,
   document.getElementById('root')
 );
